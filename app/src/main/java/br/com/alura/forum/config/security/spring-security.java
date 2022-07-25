7a73fc5250b0bd18d729959c16f00fd882a04e96
Alex Silva | Stone <alex.odilon@stone.com.br>
2022-07-25 11:26:24 -0300

    att spring security

--- app/src/main/java/br/com/alura/forum/config/security/spring-security.java
+++
@@ -1,9 +0,0 @@
-@Configuration
-public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/actuator/**").hasRole("ACTUATOR")
-                .anyRequest().permitAll();
-    }
-}
